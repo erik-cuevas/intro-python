b6c18ce1370cad6ea3680b7e63102ec5e1605c00
Erik Cuevas <erick.2023jjo@gmail.com>
2025-08-28 21:40:22 -0400

    prueba de agregacion sandwich en main

---
+++ app-venta/principal.py
@@ -0,0 +1,12 @@
+from Sandwich import Sandwich
+from Venta import Venta
+from SandwichCarne import SandwichCarne
+from SandiwchVegetariano import SandwichVegetariano
+
+mi_sandwich = Sandwich("integral", 1200)
+venta = Venta()
+vegetariano = SandwichVegetariano("integral", 2000, "tomate lechuga")
+carne = SandwichCarne("frances", 2500, "cheddar")
+
+venta.agregar_sandwich(vegetariano)
+venta.agregar_sandwich(carne)
