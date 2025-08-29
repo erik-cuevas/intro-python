351c6bb5ae4498fb29fa4232264258f78d55e9f3
Erik Cuevas <erick.2023jjo@gmail.com>
2025-08-28 21:50:12 -0400

    set precios en principal

--- app-venta/principal.py
+++ app-venta/principal.py
@@ -8,5 +8,8 @@ venta = Venta()
 vegetariano = SandwichVegetariano("integral", 2000, "tomate lechuga")
 carne = SandwichCarne("frances", 2500, "cheddar")
 
+
+carne.set_precio(4500)
+
 venta.agregar_sandwich(vegetariano)
 venta.agregar_sandwich(carne)
